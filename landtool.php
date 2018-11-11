6ffd60244031fd4cf35182fd0f9a695792905669
Peter Gloor <petergloor@users.noreply.github.com>
2018-11-11 19:18:20 +0100

    Cleaned up the code a little
    Cleaned some comments for better readability.

--- landtool.php
+++ landtool.php
@@ -37,11 +37,11 @@
 # SETTINGS
 #
 // Modify this according to your configuration.
-$dbsys = "mysql";				        // Only mysql has been tested.
-$dbhost = "127.0.0.1";			    // Usually the localhost IP address.
+$dbsys = "mysql";				// Only mysql has been tested.
+$dbhost = "127.0.0.1";			// Usually the localhost IP address.
 $dbname = "opensimmaster";	    // Database name (usually for Robust).
-$dbuser = "root";				        // Database user name.
-$dbpass = "passw0rd";		        // Database user password.
+$dbuser = "root";				// Database user name.
+$dbpass = "passw0rd";		    // Database user password.
 $sysurl = "http://example.com"; // URL to further explanations returned in case of errors.
 // End of modifiable settings.
 
@@ -51,7 +51,7 @@ $sysurl = "http://example.com"; // URL to further explanations returned in case 
 // validate_user returns true if a match has been found, otherwise false.
 function validate_user($agent_id, $s_session_id) {
     global $dbsys, $dbhost, $dbuser, $dbpass, $dbname;
-	$valid = FALSE;
+    $valid = FALSE;
 
     try {
         // Connect to database
@@ -66,17 +66,17 @@ function validate_user($agent_id, $s_session_id) {
         $query = $pdo->prepare('Select UserID From Presence 
 						Where UserID = ? And SecureSessionID = ?');
 
-        // Execute the query and validate the user if the 
-		// query is successful and an entry has been found.
+        // Execute the query and validate the user if the
+        // query is successful and an entry has been found.
         $res = $query->execute(array($agent_id, $s_session_id));
-		$valid = ($res && $query->rowCount() > 0);
+        $valid = ($res && $query->rowCount() > 0);
 
     } catch (PDOException $e) {
         die('ERROR: '.$e->getMessage());
     } catch (Exception $e) {
         die('ERROR: '.$e->getMessage());
     }
-	// return the result (boolean)
+    // return the result (boolean)
     return $valid;
 }
 
@@ -132,12 +132,12 @@ function buy_land_prep($method_name, $params, $app_data) {
             'errorMessage' => "\n\nUnable to Authenticate\n\nClick URL for more info.",
             'errorURI' => $sysurl));
     }
-    
-	// Add a header and send the response.
-	header('Content-Type: text/xml');
-  print $response_xml;
-	
-  return;
+
+    // Add a header and send the response.
+    header('Content-Type: text/xml');
+    print $response_xml;
+
+    return;
 }
 
 #
